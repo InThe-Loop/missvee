430fbe3c22977bd1a95fce547c37078ba01f4268
itwizz26 <itwizz26@gmail.com>
2022-10-25 14:38:50 +0200

    MissVee: updating cookie consent to pop-up modal.

--- public/jquery-cookie/cookie-consent.js
+++ public/jquery-cookie/cookie-consent.js
@@ -1,19 +1,20 @@
 $(function() {
-    cookiesPolicyBar();
-});
-
-function cookiesPolicyBar() {
     // Check cookie 
-    if ($.cookie('missvee') != "active") $('#cookieAcceptBar').show();
+    if ($.cookie('missvee') != "active") {
+        // $('#cookieAcceptBar').show();
+        $('#cookieModal').modal('show');
+    }
     // Assign cookie on click
     $('#cookieAcceptBarConfirm').on('click',function(){
         $.cookie('missvee', 'active', {
           path: "/",
           secure: false,
           expires: 365,
-          testing: true,
           domain: '.missveefamouslook.store'
         });
-        $('#cookieAcceptBar').fadeOut();
+        $('#cookieModal').modal('hide');
     });
-}
+    $('.close-modal').on('click',function(){
+        $('#cookieModal').modal('hide');
+    });
+});
--- resources/views/partials/cookies.blade.php
+++ resources/views/partials/cookies.blade.php
@@ -1,4 +1,32 @@
-<div id="cookieAcceptBar" class="cookieAcceptBar">
- 	Before you browse our website, please accept our <a data-toggle="modal" data-target="#privacyModal" href="#">cookies policy</a><br />
-	<button id="cookieAcceptBarConfirm" class="btn btn-secondary no-border mt-3">Accept and close</button>
+<div class="modal fade" id="cookieModal" tabindex="-1" role="dialog" aria-labelledby="cookieModal" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title text-dark" id="myModalLabel">Cookie Consent</h4>
+                <button type="button" class="close text-dark close-modal" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-dark">
+				<p>Before you start browsing our website, please read and accept this clause on cookies.</p>
+				<p><strong>What are cookies</strong></p>
+				<p>A cookie is a small piece of information stored on your computer or smart phone by the web browser. The two types of cookies used
+				on the Website are described below.</p>
+				<p><strong>"Session cookies":</strong> These are used to maintain a so-called 'session state' and
+				only lasts for the duration of your use of the Website. A session cookie expires
+				when you close your browser, or if you have not visited the server for a certain period of time. Session cookies are required for the Platform to function
+				optimally, but are not used in any way to identify you personally.</p>
+				<p><strong>"Permanent cookies":</strong> These cookies permanently store a unique code on your
+				computer or smart device hard drive in order to identify you as an individual
+				user. No Personal Information is stored in permanent cookies. You can view permanent cookies by looking in the cookies directory of your browser
+				installation. These permanent cookies are not required for the website to
+				work, but may enhance your browsing experience.</p>
+			</div>
+            <div class="modal-footer">
+				<button type="button" class="btn btn-secondary no-border mt-3 close-modal">I don't accept</button>
+				<button type="button" id="cookieAcceptBarConfirm" class="btn btn-primary no-border mt-3">I accept</button>
+				<!-- <button type="button" class="btn btn-default text-dark" data-dismiss="modal">Close</button> -->
+            </div>
+        </div>
+    </div>
 </div>
